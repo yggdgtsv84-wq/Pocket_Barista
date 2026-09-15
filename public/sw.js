c40a492db92dadfb741036ccf1de4e78984ef221
yggdgtsv84-wq <yggdgtsv84@privaterelay.appleid.com>
2026-09-16 05:08:21 +1000

    Bust stale PWA cache after brew fixes

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,17 @@
-const CACHE = 'pocket-barista-v1';
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.add('./'))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+const CACHE = 'pocket-barista-v2';
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.add('./')));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then(r => r || caches.match('./'))));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./'))));
 });
